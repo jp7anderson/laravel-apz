d783216f072e054fcfeadfb95bdf75248bd36dc5
andersonwa <andersonwebwa@hotmail.com>
2015-07-14 23:13:44 -0300

    Add articles views

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -5,33 +5,67 @@ namespace App\Http\Controllers;
 use App\Http\Requests\ArticleRequest;
 use App\Http\Controllers\Controller;
 use App\Article;
+use Request;
 
 class ArticlesController extends Controller
 {
 
     public function index()
     {
-        return Article::all();
+        $articles = Article::all();
+
+        if (Request::wantsJson()) {
+            return $articles;
+        } else {
+            return view('articles.index', compact('articles'));
+        }
     }
 
-        public function store(ArticleRequest $request)
+    public function create()
     {
-        return Article::create($request->all());
+        $article = new Article;
+
+        return view('articles.create', compact('article'));
+    }
+
+    public function store(ArticleRequest $request)
+    {
+        $article = Article::create($request->all());
+
+        if (Request::wantsJson()) {
+            return $article;
+        } else {
+            return redirect('articles');
+        }
     }
 
     public function show(Article $article)
     {
-        return $article;
+        if (Request::wantsJson()) {
+            return $article;
+        } else {
+            return view('articles.show', compact('article'));
+        }
+    }
+
+    public function edit(Article $article)
+    {
+        return view('articles.edit', compact('article'));
     }
 
     public function update(ArticleRequest $request, Article $article)
     {
-        return $article->update($request->all());
+         $deleted = $article->delete();
+
+        if (Request::wantsJson()) {
+            return (string) $deleted;
+        } else {
+            return redirect('articles');
+        }
     }
 
     public function destroy(Article $article)
     {
         return (string) $article->delete();
     }
-
 }
