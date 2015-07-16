b7778ac8274398e327ef46aead7ad87a90fc2967
andersonwa <andersonwebwa@hotmail.com>
2015-07-16 08:32:46 -0300

    Add mailer to recommend article

---
+++ app/Http/Requests/RecommendationRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+
+class RecommendationRequest extends Request
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'email' => 'required|email'
+        ];
+    }
+}
